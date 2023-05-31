d6390819e6b7f8b6e45d332e1167d0ba4833ae1c
Matthew Phan <mttphan261@gmail.com>
2023-05-31 07:31:08 -0500

    attempt2 complete

--- lib/classes/coffee.py
+++ lib/classes/coffee.py
@@ -1,17 +1,36 @@
 class Coffee:
     def __init__(self, name):
         self.name = name
+        self.orders_list = []
+        self._customer_list = []
+
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and not hasattr(self, '_name'):
+            self._name = name
+        else:
+            raise Exception
         
     def orders(self, new_order=None):
         from classes.order import Order
-        pass
+        if new_order and isinstance(new_order, Order):
+            self.orders_list.append(new_order)
+        return self.orders_list
     
     def customers(self, new_customer=None):
         from classes.customer import Customer
-        pass
+        if new_customer and isinstance(new_customer, Customer) and new_customer not in self._customer_list:
+            self._customer_list.append(new_customer)
+        return self._customer_list
     
     def num_orders(self):
-        pass
+        return len(self.orders_list)
     
     def average_price(self):
-        pass
+        price_list = []
+        for each in self.orders_list:
+            price_list.append(each.price)
+        return sum(price_list) / len(price_list)
--- lib/classes/customer.py
+++ lib/classes/customer.py
@@ -1,11 +1,38 @@
+from classes.coffee import Coffee
+from classes.order import Order
+
 class Customer:
     def __init__(self, name):
         self.name = name
-        
+        self.order_list = []
+        self._coffee_list = []
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 15:
+            self._name = name
+        else:
+            raise Exception
+
     def orders(self, new_order=None):
-        from classes.order import Order
-        pass
-    
+
+        if new_order and isinstance(new_order, Order):
+            self.order_list.append(new_order)
+        return self.order_list
+
     def coffees(self, new_coffee=None):
-        from classes.coffee import Coffee
-        pass
+
+        if new_coffee and isinstance(new_coffee, Coffee) and new_coffee not in self._coffee_list:
+            self._coffee_list.append(new_coffee)
+        return self._coffee_list
+
+    def create_order(self, coffee, price):
+
+        if isinstance(coffee, Coffee) and isinstance(price, int):
+            new_order = Order(self, coffee, price)
+            self.orders(new_order)
+            return new_order
